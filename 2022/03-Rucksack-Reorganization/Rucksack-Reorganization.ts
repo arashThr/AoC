0948c1210a81e8956ef004c1ec5e0de6ca02fcd1
Arash Taher <arashThr@outlook.com>
2022-12-05 19:31:21 +0100

    Day 3 - Part 2

--- 2022/03-Rucksack-Reorganization/Rucksack-Reorganization.ts
+++ 2022/03-Rucksack-Reorganization/Rucksack-Reorganization.ts
@@ -5,21 +5,30 @@
 import { createReadStream } from 'fs'
 import { createInterface } from 'readline'
 import { join as pathJoin } from 'path'
+import { strict as assert } from 'assert'
 
-let total = 0
+let part1Total = 0
+const lines: string[] = []
 
-function processLine(l: string) {
-    const digits = l.split('').map(c => c.toLowerCase() == c
+function calcPriority(l: string) {
+    return l.split('').map(c => c.toLowerCase() == c
         ? c.charCodeAt(0) - 'a'.charCodeAt(0) + 1
         : c.charCodeAt(0) - 'A'.charCodeAt(0) + 27)
-
-    total += findCommon(digits)
 }
 
-function findCommon(digits: number[]): number {
-    const mid = digits.length / 2
-    const small = digits.slice(0, mid)
-    const big = digits.slice(mid)
+function part1(l: string) {
+    const priorities = calcPriority(l)
+    part1Total += findCommon(priorities)
+}
+
+function part2(l: string) {
+  lines.push(l)
+}
+
+function findCommon(priorities: number[]): number {
+    const mid = priorities.length / 2
+    const small = priorities.slice(0, mid)
+    const big = priorities.slice(mid)
 
     for (let i = 0; i < mid; i++) {
         for (let j = 0; j < mid * 2; j++) {
@@ -30,14 +39,32 @@ function findCommon(digits: number[]): number {
     throw Error('Common not found')
 }
 
-function findAnswer() {
-    console.log('Totoal: ' + total)
+function findPart1Answer() {
+    console.log('Total: ' + part1Total)
+}
+
+function findPart2Answer() {
+  assert(lines.length % 3 === 0)
+  const alphabet = [...'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ']
+  let total = 0
+
+  for (let start = 0; start < lines.length; start+= 3) {
+    const group = lines.slice(start, start + 3)
+    for (let a of alphabet) {
+      const hasItem = group.every(member => member.includes(a))
+      if (hasItem) {
+        total += calcPriority(a)[0]
+        break
+      }
+    }
+  }
+  console.log('Total for part 2: ' + total)
 }
 
 const filePath = pathJoin(__dirname, process.argv[2])
 createInterface({
     input: createReadStream(filePath)
 })
-.on('line', processLine)
-.on('close', findAnswer)
+.on('line', part2)
+.on('close', findPart2Answer)
 
