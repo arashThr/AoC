8ffe4391b4d1fef6b51695e3d3b3e95eaab39a16
Arash Taher <arashThr@outlook.com>
2023-01-08 00:07:41 +0100

    Day 8 - part 2

--- 2022/08-Treetop-Tree-House/Treetop-Tree-House.ts
+++ 2022/08-Treetop-Tree-House/Treetop-Tree-House.ts
@@ -56,6 +56,52 @@ function findAnswer() {
     console.log(visibles.size)
 }
 
+class Score {
+    up = 0
+    down = 0
+    left = 0
+    right = 0
+}
+
+function findAnswer2() {
+    const len = trees.length
+
+    let scores: Score[] = []
+
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            let score = new Score()
+            for (let n = i - 1; n >= 0; n--) {
+                score.up += 1
+                if (trees[n][j] >= trees[i][j]) break
+            }
+
+            for (let n = i + 1; n < len; n++) {
+                score.down += 1
+                if (trees[n][j] >= trees[i][j]) break
+            }
+
+            for (let n = j - 1; n >= 0; n--) {
+                score.left += 1
+                if (trees[i][n] >= trees[i][j]) break
+            }
+
+            for (let n = j + 1; n < len; n++) {
+                score.right += 1
+                if (trees[i][n] >= trees[i][j]) break
+            }
+            scores.push(score)
+        }
+    }
+
+    let max = 0
+    for (let s of scores) {
+        const val = s.up * s.down * s.left * s.right
+        if (val > max) max = val
+    }
+    console.log('Max: ', max)
+}
+
 function showResult(visible: Set<string>, len: number) {
     let arr: string[][] = []
     for (let i = 0; i < len; i++) {
@@ -72,5 +118,5 @@ createInterface({
     input: createReadStream(filePath)
 })
 .on('line', processLine)
-.on('close', findAnswer)
+.on('close', findAnswer2)
 
